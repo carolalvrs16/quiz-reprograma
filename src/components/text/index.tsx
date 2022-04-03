4db1aa7fef219f39b4b711ee9d3da6e466d3740a
Carolina Alves <carolalvrs@hotmail.com>
2022-04-03 10:15:59 -0300

    feat(page): home

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -15,12 +15,14 @@ const Text: React.FC<TextHandles> = ({
     className,
     type === 'p' ? 'text-base' : '',
     type === 'h1' ? 'text-5xl !leading-[3.5rem]' : '',
+    type === 'h3' ? 'text-2xl' : '',
   );
 
   const Typography = useCallback(() => {
     if (type === 'p') return <p {...rest} className={styled}>{label}</p>;
     if (type === 'h1') return <h1 {...rest} className={styled}>{label}</h1>;
     if (type === 'h2') return <h2 {...rest} className={styled}>{label}</h2>;
+    if (type === 'h3') return <h3 {...rest} className={styled}>{label}</h3>;
     if (type === 'span') return <span {...rest} className={styled}>{label}</span>;
 
     return <p />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,54 +8,79 @@ import {
 } from '../components';
 
 const Home: React.FC = () => {
+  const [date, onDate] = useState(null);
+  const [next, onNext] = useState(false);
   const [selected, onSelected] = useState('');
-  const [startDate, onStartDate] = useState(null);
-
-  console.log(selected);
 
   return (
     <div className="home">
-      <div className="home-container">
-        <div className="home-title">
-          <Text
-            label="Olá,"
-            type="h1"
-          />
+      {!next ? (
+        <div className="home-container">
+          <div className="home-title">
+            <Text
+              label="Olá,"
+              type="h1"
+            />
+
+            <Text
+              label="Vamos ajudar você a
+            encontrar o curso ideal."
+              type="h1"
+            />
+          </div>
 
           <Text
-            label="Vamos ajudar você a
-            encontrar o curso ideal."
-            type="h1"
+            label="Preencha as informações abaixo, é rápido e fácil."
+            className="text-gray-400 mt-6 mb-14"
+          />
+
+          <Select
+            label="Identitade de Gênero"
+            options={['Cis', 'Trans']}
+            selected={selected}
+            className="mb-8"
+            onSelected={onSelected}
+            placeholder="Selecione uma opção"
+          />
+
+          <Datepicker
+            label="Data de nascimento"
+            start={date}
+            onStart={onDate}
+            placeholder="Selecione uma data"
+          />
+
+          <Button
+            label="Próximo"
+            onClick={() => onNext(true)}
+            disabled={!selected || onDate === null}
+            className="w-full mt-20"
           />
         </div>
+      ) : (
+        <div>
+          <Text
+            type="h3"
+            label="Acesse sua conta e"
+          />
+          <Text
+            label="Vamos começar"
+            type="h1"
+            className="mt-3"
+          />
+          <Text
+            label="O quiz irá avaliar seu perfil e informar
+            o melhor curso para você."
+            className="text-gray-400 mt-6 mb-14 max-w-[23rem]"
+          />
 
-        <Text
-          label="Preencha as informações abaixo, é rápido e fácil."
-          className="text-gray-400 mt-6 mb-14"
-        />
-
-        <Select
-          label="Gênero"
-          options={['Cis', 'Trans']}
-          selected={selected}
-          className="mb-8"
-          onSelected={onSelected}
-          placeholder="Selecione uma opção"
-        />
-
-        <Datepicker
-          label="Data de nascimento"
-          start={startDate}
-          onStart={onStartDate}
-          placeholder="Selecione uma data"
-        />
-
-        <Button
-          label="Próximo"
-          disabled
-          className="w-full mt-20"
-        />
-      </div>
+          <Button
+            label="Entrar"
+            onClick={() => onNext(true)}
+            className="w-full max-w-[28rem]"
+          />
+        </div>
+      )}
     </div>
   );
 };
